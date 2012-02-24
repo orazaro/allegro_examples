adaa2845d5a7f07438498df1251eec3be2c21ef8
orazaro <oraz@rambler.ru>
2012-02-24 23:11:33 +0400

    first commit

---
+++ Makefile
@@ -0,0 +1,28 @@
+#$Id: Makefile,v 1.2 2012/02/08 16:56:33 oraz Exp $
+
+CPP			= $(wildcard *.cpp)
+PROGRAMS	= $(CPP:%.cpp=%) 
+CFILES		= $(wildcard *.c)
+PROGRAMC	= $(CFILES:%.c=%) 
+LIBRARIES	= cppunit allegro allegro_main
+DEPS		= cppunit_run.inc
+
+CC          = gcc
+CXX         = g++
+CFLAGS      += -Wall -O3
+CPPFLAGS    += -Wall -O2
+LDFLAGS     = $(LIBRARIES:%=-l%)
+
+.PHONY: all clean
+
+all: $(PROGRAMS) $(PROGRAMC) 
+
+%: %.cpp $(DEPS) 
+	$(CXX) $(CPPFLAGS) $(LDFLAGS) $< -o $@
+
+%: %.c
+	$(CC) $(CFLAGS) $(LDFLAGS) $^ -o $@
+
+clean:
+	rm -f *.o *.a $(PROGRAMS) $(PROGRAMC) cpptestresults.xml
+
---
+++ display.cpp
@@ -0,0 +1,33 @@
+#include <allegro5/allegro.h>
+#include <iostream>
+
+int test_allegro()
+{
+    ALLEGRO_DISPLAY *display = NULL;
+ 
+    if(!al_init()) {
+        std::cerr << "failed to initialize allegro!\n";
+        return -1;
+    }
+
+    display = al_create_display(640, 480);
+    if(!display) {
+        std::cerr << "failed to create display!\n";
+        return -1;
+    }
+
+    al_clear_to_color(al_map_rgb(0xAA,0,0));
+    al_flip_display();
+    al_rest(5.0);
+    al_destroy_display(display);
+ 
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    test_allegro();
+
+    return 0;
+}
